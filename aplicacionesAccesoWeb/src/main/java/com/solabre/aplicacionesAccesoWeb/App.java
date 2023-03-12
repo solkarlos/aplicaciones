f33810eaaba7d5b11dc3fed4210fdea898b944ec
Carlos Solabre <carlossolabre@gmail.com>
2023-03-12 20:50:59 +0100

    no message

---
+++ aplicacionesAccesoWeb/src/main/java/com/solabre/aplicacionesAccesoWeb/App.java
@@ -0,0 +1,13 @@
+package com.solabre.aplicacionesAccesoWeb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
